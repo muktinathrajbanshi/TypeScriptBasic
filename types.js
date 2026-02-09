d5431507ba045f76788c2c06162becc45816f66d
unknown <muktinathrajbanshi786@gmail.com>
2026-02-09 09:14:04 +0545

    Variable Declaration in Typescript

--- types.js
+++ types.js
@@ -1,1 +1,7 @@
-console.log("hello muktinath");
+function subscribe() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("finally out side the block " + i);
+}
+subscribe();
--- types.ts
+++ types.ts
@@ -1,1 +1,8 @@
-console.log("hello muktinath");
+function subscribe() {
+    for (var i = 0; i<5; i++) {
+        console.log(i);
+    }
+    console.log("finally out side the block " +i);
+}
+
+subscribe();
