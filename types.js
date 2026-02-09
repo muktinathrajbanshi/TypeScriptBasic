40d5d8bdbfa95250fc13dac246eb49a2e2d4405d
unknown <muktinathrajbanshi786@gmail.com>
2026-02-09 09:57:28 +0545

    Types in Typescript

--- types.js
+++ types.js
@@ -1,7 +1,22 @@
-function subscribe() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log("finally out side the block " + i);
-}
-subscribe();
+// function subscribe() {
+//     for (var i = 0; i<5; i++) {
+//         console.log(i);
+//     }
+//     console.log("finally out side the block " +i);
+// }
+// subscribe();
+// let myNum : number;
+// let myNum = 1;l
+// let myStr : string;
+// myStr = "Welcome to my Code";
+// let myBool : boolean;
+// myBool = true;
+// let myAnyType : any;
+// myAnyType = "hello";
+// let arrStr : string[];
+// arrStr = ["hello1", "hello2"];
+// let arrNum : number[];
+// arrNum = [1,2,3,4,5];
+var anytype;
+anytype = "check this out";
+console.log(anytype);
--- types.ts
+++ types.ts
@@ -1,8 +1,31 @@
-function subscribe() {
-    for (var i = 0; i<5; i++) {
-        console.log(i);
-    }
-    console.log("finally out side the block " +i);
-}
+// function subscribe() {
+//     for (var i = 0; i<5; i++) {
+//         console.log(i);
+//     }
+//     console.log("finally out side the block " +i);
+// }
 
-subscribe();
+// subscribe();
+
+// let myNum : number;
+// let myNum = 1;l
+
+// let myStr : string;
+// myStr = "Welcome to my Code";
+
+// let myBool : boolean;
+// myBool = true;
+
+// let myAnyType : any;
+// myAnyType = "hello";
+
+// let arrStr : string[];
+// arrStr = ["hello1", "hello2"];
+
+// let arrNum : number[];
+// arrNum = [1,2,3,4,5];
+
+let anytype;
+anytype = "check this out";
+
+console.log(anytype);
