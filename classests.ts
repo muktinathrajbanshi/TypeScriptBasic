943dd9276abac271dd524573f81b426082e90f50
unknown <muktinathrajbanshi786@gmail.com>
2026-02-09 14:59:55 +0545

    Object in Typescript

--- classests.ts
+++ classests.ts
@@ -1,16 +1,32 @@
 //?cohesive
 
-class youtube{
+class youtubes{
 
     firstName : string;
     lastName : string;
     age : number; 
 
     getFirstName() {
-        //
+        console.log("My first name is " + this.firstName);
+        
     }
 
     getLastName() {
         //
     }
 }
+
+let ytObj = new youtubes();
+ytObj.firstName = "Muktinath";
+ytObj.getFirstName();
+
+
+
+
+
+//? Creating Instance objects
+//? To create an instance of the class, use the new keyword
+//? followed by the class name. The syntax for the same is given below-
+
+//?! Syntax
+//? var object_name = new class_name();
