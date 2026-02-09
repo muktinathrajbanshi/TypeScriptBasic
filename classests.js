7542668adc7bee805ad5c160d3a6523bc7c165e7
unknown <muktinathrajbanshi786@gmail.com>
2026-02-09 15:38:41 +0545

    Access Modifiers in TypeScript

--- classests.js
+++ classests.js
@@ -1,36 +1,26 @@
 //?cohesive
-
-class youtubes{
-
+var youtubes = /** @class */ (function () {
     //? The this keyword refers to the current instance of the class.
     //? Here, the parameter name and the name of the class's field are the same.
     //? Hence to avoid ambiguity, the class's field is prefixed with the this keyword.
-
-    constructor(public firstName : string, private age  : number, private lastName ? : string) {
-        
+    function youtubes(firstName, age, lastName) {
+        this.firstName = firstName;
+        this.age = age;
+        this.lastName = lastName;
     }
-
-    getFirstName() {
-        console.log("My first name is " + this.firstName + " "+ 
+    youtubes.prototype.getFirstName = function () {
+        console.log("My first name is " + this.firstName + " " +
             this.age);
-        
-    }
-
-    getLastName() {
+    };
+    youtubes.prototype.getLastName = function () {
         //
-    }
-}
-
-let ytObj = new youtubes("Muktinath", 23);
+    };
+    return youtubes;
+}());
+var ytObj = new youtubes("Muktinath", 23);
 ytObj.getFirstName();
-
-
-
-
-
 //? Creating Instance objects
 //? To create an instance of the class, use the new keyword
 //? followed by the class name. The syntax for the same is given below-
-
 //?! Syntax
 //? var object_name = new class_name();
